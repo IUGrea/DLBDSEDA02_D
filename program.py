c7a383106f627ebe4fd708570211d09254322734
IUGrea <alexander.greussing1@iu-study.org>
2025-09-10 19:17:49 +0200

    Implement vecorization and topic extraction methods

--- program.py
+++ program.py
@@ -5,6 +5,11 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.decomposition import TruncatedSVD
+import matplotlib.pyplot as plt
 
 def remove_urls(dataset: str):
     # Regex pattern to match URLs (http, https, www)
@@ -42,12 +47,62 @@ def preprocess (dataset: str):
     preprocdataset = lemmatize_tokens(preprocdataset)
     return preprocdataset
 
+def newbowmatrix (docs: list):
+    texts = [" ".join(doc) for doc in docs]
+    vect = CountVectorizer()
+    X = vect.fit_transform(texts)
+    return vect, X
+
+def newtfidfmatrix(docs: list):
+    texts = [" ".join(doc) for doc in docs]
+    vect = TfidfVectorizer()
+    X = vect.fit_transform(texts)
+    return vect, X
+
+def ldatopicextraction (vect,matrix,texts,topiccount,relevanttopic):
+    def print_topics(feature_names, n_top_words):
+        for idx, topic in enumerate(lda.components_):
+            top_words = [feature_names[i] for i in topic.argsort()[-n_top_words:]]
+            print(f"Topic {idx}: {', '.join(top_words)}")
+    lda = LatentDirichletAllocation(n_components=topiccount, random_state=42)
+    lda.fit(matrix)
+    feature_names = vect.get_feature_names_out()
+    
+    doc_topic_dist = lda.transform(X)
+    for i, complaint in enumerate(texts):
+        topic_id = doc_topic_dist[i].argmax()
+        print(f"Complaint: '{complaint}' → Topic {topic_id}")
+
+
+    print_topics(feature_names, relevanttopic)
+
+def lsatopicextraction(vect, matrix, texts, topiccount=5, n_top_words=8):
+    lsa = TruncatedSVD(n_components=topiccount, random_state=42)
+    lsa.fit(matrix)
+    feature_names = vect.get_feature_names_out()
+    
+    for idx, topic in enumerate(lsa.components_):
+        top_words = [feature_names[i] for i in topic.argsort()[-n_top_words:][::-1]]
+        print(f"Topic {idx}: {', '.join(top_words)}")
+
 complaints = []
+tokens = []
+vectormethod = ""
+topicmethod = ""
 customstopwords = ("xxxx","xxxxxxxx")
 
 print("Enter Path to input JSON")
 jsonfile = input()
 jsonfile = jsonfile.replace('"', '').replace("'", "")
+
+
+while vectormethod != 1 and vectormethod != 2:
+    print("Choose Vectorization method: 1=Bag of Words (Bow),2=Term Frequency-Inverse Document Frequency (TF-IDF)")
+    vectormethod = int(input())
+
+while topicmethod != 1 and topicmethod != 2:
+    print("Choose topic extraction method: 1=Latent Semantic Analysis (LSA),2=Latent Dirichlet Allocation (LDA)")
+    topicmethod = int(input())    
 
 # Download NLTK Data and initialize
 nltk.download("punkt")
@@ -68,6 +123,15 @@ for obj in data:
     if len(obj["complaint_what_happened"]) != 0:
         complaints.append(obj["complaint_what_happened"])
 
-for obj in complaints:
-    print(preprocess(obj))
-    print("")
+for complaint in complaints:
+    tokens.append(preprocess(complaint))
+
+if vectormethod == 1:
+    vectorizer, X = newbowmatrix(tokens)
+if vectormethod == 2:
+    vectorizer, X = newtfidfmatrix(tokens)
+
+if topicmethod == 1:
+    lsatopicextraction(vectorizer,X,tokens,6,8)
+if topicmethod == 2:
+    ldatopicextraction(vectorizer,X,tokens,6,8)
